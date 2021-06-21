d8cd0725ecbd7a9795b0933cf24a98c10b620c7f
rtamburro <ronnie.tamburro@protonmail.com>
2021-06-21 19:56:37 -0400

    first commit

--- script.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelectorAll("ul li");
-items.forEach(item => {
-  item.addEventListener("click", () => {
-    document.querySelector("li.active").classList.remove("active");
-    item.classList.add("active");
-  })
-});
